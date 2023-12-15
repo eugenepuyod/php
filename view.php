0a4cb850235274582e81b1950f45dd59cdbd332c
eugenepuyod <eugenerpuyod@gmail.com>
2023-12-15 14:25:54 +0800

    Create view.php

---
+++ view.php
@@ -0,0 +1,16 @@
+<?php
+require "Model/User.php";
+$user = new User;
+
+$item = ['name','email','pass'];
+$value = ['Chelsey','chelsey@test.com','test123'];
+$insertId = $user->insertid($item, $value);
+var_dump($insertId);
+
+$setNames = array(
+    'name' => 'Jocelyn',
+    'email' => 'new@test.com'
+);
+var_dump($user->update(7, $setNames));
+
+?>
